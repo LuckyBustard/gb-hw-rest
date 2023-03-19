6d241fb4f143684f4b153815f050c1e68b4ce02e
Sergey Mytsykov <smytsykov@awg.ru>
2023-03-20 01:45:24 +0300

    Задание 8

--- app/todo/tests.py
+++ app/todo/tests.py
@@ -1,3 +1,35 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from .views import ProjectsModelViewSet
+from .models import Project, Task
 
-# Create your tests here.
+
+class TestAuthorViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectsModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        project = Project.objects.create(name='name1', repository_link='rl')
+        client = APIClient()
+        response = client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestProject(APITestCase):
+    def test_get_list(self):
+        response = self.client.get('/api/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        task = mixer.blend(Task, text='text')
+        response = self.client.get(f'/api/tasks/{task.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_task = json.loads(response.content)
+        self.assertEqual(response_task['text'], 'text')
